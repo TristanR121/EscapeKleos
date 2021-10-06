89f3390c8380a0b3b372bad0ec60434eff437d5e
TristanR121 <61286565+TristanR121@users.noreply.github.com>
2021-10-06 12:26:09 -0400

    update2

---
+++ Escape Kleos/Assets/ReadME.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadME : MonoBehaviour
+{
+    //assets from:
+    //https://www.emaceart.com/
+    //https://sites.google.com/view/terranovacreations/home
+    //https://www.artstation.com/kunniki
+    //https://inkphantomstudio.wordpress.com/
+    //https://www.brokenvector.com/
+}
